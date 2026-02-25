221df765faf6e41e37eca8bf00982cd63c5d832f
xianbryancolinaperalta24-byte <xianbryancolina.peralta24@bicol-u.edu.ph>
2026-02-25 20:15:34 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Nav active state
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    navLinks.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
